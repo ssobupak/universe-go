4a7cabf3190ef0b64a499c67f95e8b9676eb8a9b
andreubltzr <andreubltzr11@gmail.com>
2023-05-02 16:49:16 +0200

    cambios generales

--- index.js
+++ index.js
@@ -4,11 +4,3 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
-
-/* document.querySelector(".login-btn").addEventListener("click", () => {
-  document.querySelector(".popup").classList.add("active");
-});
-
-document.querySelector(".popup .close-btn").addEventListener("click", () => {
-  document.querySelector(".popup").classList.remove("active");
-}); */
