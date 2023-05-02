40215aebff295e04ad68af05daa626b275c7e912
andreubltzr <andreubltzr11@gmail.com>
2023-05-02 17:21:40 +0200

    popup login añadido

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
+
+const modal = document.querySelector("#modal");
+const openModal = document.querySelector(".open-modal");
+const closeModal = document.querySelector(".close-modal");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
